9df76b90510d90950cbe4f55d291451c204ad423
muta.yutaro@aainc.co.jp <muta.yutaro@aainc.co.jp>
2015-07-29 12:19:45 +0900

    Added .oh-my-zsh and Fixed dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,6 +3,7 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -sf ~/dotfiles/.pyenv ~/.pyenv
 ln -sf ~/dotfiles/.tmux-reattach-to-user-namespace.conf ~/.tmux-reattach-to-user-namespace.conf
+ln -sf ~/dotfiles/tmux-powerline ~/tmux-powerline
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
